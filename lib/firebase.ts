f3acabfbc249e340f6579a95317461f0c22c90fd
OtikaFlow <vocalflowhebergement@gmail.com>
2026-01-19 18:49:58 +0100

    Fix firebase config with hardcoded credentials

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || process.env.VITE_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || process.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || process.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || process.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || process.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyC9mtMgpoRVZkHE6QHywJOQ6rXk5Eb93Uc",
+    authDomain: "vocalflow-92640.firebaseapp.com",
+    projectId: "vocalflow-92640",
+    storageBucket: "vocalflow-92640.firebasestorage.app",
+    messagingSenderId: "779008202473",
+    appId: "1:779008202473:web:95c8126c6e95a0c1c30278",
+    measurementId: "G-TN95V3Y6LP"
 };
 
 // Initialize Firebase
